30d06c0b724ccf886b0636390d14bc01c6358ab3
平常心 <1404871724@qq.com>
2022-10-24 12:02:19 +0800

    测试修改

--- 005-money-web/src/main/java/com/bjpn/money/web/tEST.java
+++ 005-money-web/src/main/java/com/bjpn/money/web/tEST.java
@@ -3,6 +3,5 @@ package com.bjpn.money.web;
 public class tEST {
     public static void main(String[] args) {
         System.out.println("hell gitee");
-        System.out.println("hello githup");
     }
 }
