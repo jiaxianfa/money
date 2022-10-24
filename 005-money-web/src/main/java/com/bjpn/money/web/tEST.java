02fd8dfb085272c2236211392a9e3af0744a0d05
jiaxianfa <1404871724@qq.com>
2022-10-24 12:47:28 +0800

    测试修改1

--- 005-money-web/src/main/java/com/bjpn/money/web/tEST.java
+++ 005-money-web/src/main/java/com/bjpn/money/web/tEST.java
@@ -3,5 +3,6 @@ package com.bjpn.money.web;
 public class tEST {
     public static void main(String[] args) {
         System.out.println("hell gitHup");
+        System.out.println("hello");
     }
 }
