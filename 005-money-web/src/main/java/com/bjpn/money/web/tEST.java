dcb7f050466440da962e32e4c7f664ff8600fe05
jiaxianfa <1404871724@qq.com>
2022-10-24 12:50:12 +0800

    第一次上传

--- 005-money-web/src/main/java/com/bjpn/money/web/tEST.java
+++
@@ -1,8 +0,0 @@
-package com.bjpn.money.web;
-
-public class tEST {
-    public static void main(String[] args) {
-        System.out.println("hell gitHup");
-        System.out.println("hello");
-    }
-}
